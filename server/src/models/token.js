50b63bdf3c91b0c6377448e9d61b3fc4c70327dd
Radeonxz <xuanxxzhao@gmail.com>
2019-05-05 00:29:02 -0400

    ..

--- server/src/models/token.js
+++ server/src/models/token.js
@@ -1,21 +1,29 @@
 import moment from 'moment';
 import { ObjectId } from 'mongodb';
+import { OrderedMap } from 'immutable';
 
 export default class Token {
   constructor(app) {
     this.app = app;
+
+    this.tokens = new OrderedMap();
   }
 
   load(id = null) {
     id = `${id}`;
     return new Promise((resolve, reject) => {
       this.findTokenById(id, (err, token) => {
+        if(!err && token) {
+          const tokenId = token._id.toString();
+          this.tokens = this.tokens.set(tokenId, token);
+        }
         return err ? reject(err) : resolve(token);
       });
     })
   }
 
   findTokenById(id, callback = () => {}) {
+    console.log('Begin to query token from db...');
     const idObject = new ObjectId(id);
     const query = {_id: idObject};
     this.app.db.collection('tokens').findOne(query, (err, result) => {
