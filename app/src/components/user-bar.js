10bddcd2dd76492e2b33524b12a67fc83e866d50
Radeonxz <xuanxxzhao@gmail.com>
2019-07-27 10:26:06 -0400

    fix register and signin form

--- app/src/components/user-bar.js
+++ app/src/components/user-bar.js
@@ -21,7 +21,7 @@ export default class UserBar extends Component {
     const isConnected = store.isConnected();
     return (
       <div className = 'user-bar'>
-        {!me && !isConnected ? <div className='app-warning-state'>Connecting...</div> : null}
+        {me && !isConnected ? <div className='app-warning-state'>Connecting...</div> : null}
         {!me ? <button onClick={() => {
           this.setState({
             showUserForm: true,
