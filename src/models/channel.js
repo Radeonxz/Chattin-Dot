b747d0ceb75cb53635a6dbed5421dcaa24d233a1
Radeonxz <xuanxxzhao@gmail.com>
2020-05-05 22:18:07 -0400

    chore: format channel

--- src/models/channel.js
+++ src/models/channel.js
@@ -1,27 +1,30 @@
-import _ from 'lodash';
-import { toString } from '../helper';
-import { ObjectID } from 'mongodb';
-import { OrderedMap } from 'immutable';
+import _ from "lodash";
+import { toString } from "../helper";
+import { ObjectID } from "mongodb";
+import { OrderedMap } from "immutable";
 
-export default class Channel{
+export default class Channel {
   constructor(app) {
     this.app = app;
     this.channels = new OrderedMap();
   }
 
   aggregate(q) {
-    return new Promise ((resolve, reject) => {
-      this.app.db.collection('channels').aggregate(q, (err, results) => {
+    return new Promise((resolve, reject) => {
+      this.app.db.collection("channels").aggregate(q, (err, results) => {
         return err ? reject(err) : resolve(results);
-      })
-    })
+      });
+    });
   }
 
   find(q, options = {}) {
-    return new Promise ((resolve, reject) => {
-      this.app.db.collection('channels').find(q, options).toArray((err, results) => {
-        return err ? reject(err) : resolve(results);
-      });
+    return new Promise((resolve, reject) => {
+      this.app.db
+        .collection("channels")
+        .find(q, options)
+        .toArray((err, results) => {
+          return err ? reject(err) : resolve(results);
+        });
     });
   }
 
@@ -31,61 +34,65 @@ export default class Channel{
       // find in cache
       const channelFromCache = this.channels.get(id);
 
-      if(channelFromCache) {
+      if (channelFromCache) {
         return resolve(channelFromCache);
       }
 
       // find in db if not found
-      this.findById(id).then((c) => {
-        this.channels = this.channels.set(id, c);
-        return resolve(c);
-      }).catch((err) => {
-        return reject(err);
-      });
+      this.findById(id)
+        .then((c) => {
+          this.channels = this.channels.set(id, c);
+          return resolve(c);
+        })
+        .catch((err) => {
+          return reject(err);
+        });
     });
   }
 
   findById(id) {
     return new Promise((resolve, reject) => {
-      this.app.db.collection('channels').findOne({_id: new ObjectID(id)}, (err, result) => {
-        if(err || !result) {
-          return reject(err ? err : 'Not Found');
-        }
-        return resolve(result);
-      });
+      this.app.db
+        .collection("channels")
+        .findOne({ _id: new ObjectID(id) }, (err, result) => {
+          if (err || !result) {
+            return reject(err ? err : "Not Found");
+          }
+          return resolve(result);
+        });
     });
   }
 
   create(obj) {
     return new Promise((resolve, reject) => {
-      let id = toString(_.get(obj, '_id'));
+      let id = toString(_.get(obj, "_id"));
       let idObject = id ? new ObjectID(id) : new ObjectID();
 
       let members = [];
 
-      _.each(_.get(obj, 'members', []), (value, key) => {
-        console.log('key', key, value);
+      _.each(_.get(obj, "members", []), (value, key) => {
+        console.log("key", key, value);
         const memberObjectId = new ObjectID(key);
         members.push(memberObjectId);
       });
 
       let userIdObject = null;
-      let userId = _.get(obj, 'userId', null);
-      if(userId) {
+      let userId = _.get(obj, "userId", null);
+      if (userId) {
         userIdObject = new ObjectID(userId);
       }
 
       const channel = {
         _id: idObject,
-        title: _.get(obj, 'title', ''),
-        lastMessage: _.get(obj, 'lastMessage', ''),
+        title: _.get(obj, "title", ""),
+        lastMessage: _.get(obj, "lastMessage", ""),
         created: new Date(),
         userId: userIdObject,
-        members: members,
-      }
+        members: members
+      };
 
-      this.app.db.collection('channels').insertOne(channel, (err, info) => {
-        if(!err) {
+      this.app.db.collection("channels").insertOne(channel, (err, info) => {
+        if (!err) {
           const channelId = channel._id.toString();
           this.channels = this.channels.set(channelId, channel);
         }
