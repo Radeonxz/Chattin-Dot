113edd33d681d8a1958a3649656e6710a0d37e8c
Radeonxz <xuanxxzhao@gmail.com>
2020-05-20 21:49:15 -0400

    chore: client2 cleanup

--- src/client2.js
+++ src/client2.js
@@ -1,17 +1,17 @@
 //import WebSocket from 'ws';
 
-const WebSocket = require('ws');
+const WebSocket = require("ws");
 
-const ws = new WebSocket('ws://localhost:3000');
+const ws = new WebSocket("ws://localhost:3000");
 
-ws.on('open', () => {
-  console.log('successful connected');
+ws.on("open", () => {
+  console.log("successful connected");
 
   //send new message from this client to server
-  ws.send('Hello server my name is client2.');
+  ws.send("Hello server my name is client2.");
 
   //listen mesage from server
-  ws.on('message', (message) => {
-    console.log('Got message back from server: ', message);
+  ws.on("message", (message) => {
+    console.log("Got message back from server: ", message);
   });
-})
+});
